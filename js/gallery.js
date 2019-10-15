d556ad9250c267e209fd301987f92fed22d3ff2a
caihua <727605760@qq.com>
2019-10-15 23:20:34 +0800

    Site updated: 2019-10-15 23:20:33

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (typeof ($.fn.lightGallery) === 'function') {
-        $('.article').lightGallery({ selector: '.gallery-item' });
-    }
-    if (typeof ($.fn.justifiedGallery) === 'function') {
-        if ($('.justified-gallery > p > .gallery-item').length) {
-            $('.justified-gallery > p > .gallery-item').unwrap();
-        }
-        $('.justified-gallery').justifiedGallery();
-    }
-});
+// build time:Tue Oct 15 2019 23:20:27 GMT+0800 (GMT+08:00)
+document.addEventListener("DOMContentLoaded",function(){if(typeof $.fn.lightGallery==="function"){$(".article").lightGallery({selector:".gallery-item"})}if(typeof $.fn.justifiedGallery==="function"){if($(".justified-gallery > p > .gallery-item").length){$(".justified-gallery > p > .gallery-item").unwrap()}$(".justified-gallery").justifiedGallery()}});
+//rebuild by neat 
